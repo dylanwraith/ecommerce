e850caf91236e7eb4a304d9b7bf93b1887f7fddc
dylanwraith <dylanwraith@gmail.com>
2025-02-05 11:57:47 -0800

    EC-5: Introduce authorization via Auth0 Roles

--- src/main/java/com/dylanwraith/ecommerce/controller/AuthController.java
+++ src/main/java/com/dylanwraith/ecommerce/controller/AuthController.java
@@ -2,16 +2,34 @@ package com.dylanwraith.ecommerce.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthController {
 
     @GetMapping("/")
-    public ResponseEntity<String> homePage(@AuthenticationPrincipal OidcUser principal) {
-        return ResponseEntity.ok(principal.getEmail());
+    public ResponseEntity<String> publicPage() {
+        return ResponseEntity.ok("This page is for all users, authenticated or not.");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> privatePage(
+            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client,
+            @AuthenticationPrincipal OidcUser principal) {
+
+        return ResponseEntity.ok("This page is for authenticated users.");
+    }
+
+    @GetMapping("/private-admin")
+    public ResponseEntity<String> privateScopedPage(
+            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client,
+            @AuthenticationPrincipal OidcUser principal) {
+
+        return ResponseEntity.ok("This page is for Admin users.");
     }
 
 }
--- src/main/java/com/dylanwraith/ecommerce/security/SecurityConfiguration.java
+++ src/main/java/com/dylanwraith/ecommerce/security/SecurityConfiguration.java
@@ -1,65 +1,4 @@
-# ecommerce
-
-### Local Development Setup
-1. Install MySQL 8.0.41
-2. Start MySQL 
-3. Create `ecommerce` MySQL table 
-4. Add environment variables for `ECOMMERCE_MYSQL_DB_USER` and `ECOMMERCE_MYSQL_DB_PASSWORD`
-5. Create MySQL user for `ECOMMERCE_MYSQL_DB_USER` and `ECOMMERCE_MYSQL_DB_PASSWORD`
-6. Run `./gradlew start` from project directory in terminal
-
-### Auth0 Setup
-1. Create Auth0 account
-2. Create Auth0 Application of type Regular Web Application
-   1. Take note of Client ID, Client Secret, and Domain
-   2. Add http://localhost:8080/login/oauth2/code/okta and http://localhost:8080/authorize/oauth2/code/okta to Allowed Callback URLs
-   3. Add http://localhost:8080 to Allowed Logout URLs
-   4. Add http://localhost:8080 to Allowed Web Origins 
-3. Create Auth0 API
-   1. Give a standard identifier like https://ecommerce-api.com, this will be your effective "audience" during authorization
-   2. Ensure JWT Profile is Auth0 and Signing Algorithm is RS256
-   3. Enable RBAC
-   4. Toggle "Add Permissions in the Access Token"
-4. Create Auth0 Roles
-   1. Create role for Admin
-   2. Create role for Customer
-      1. Ensure names begin with "ROLE_" ie "ROLE_admin"
-5. Create Auth0 Action - Trigger for Post-Login
-   1. Drag between `Start` amd `Complete`
-   2. Code for Trigger is as follows. This links roles to tokens. Namespace can be anything, will be used for mapping in Spring Security.
-    ``` 
-      exports.onExecutePostLogin = async (event, api) => {
-          const namespace = 'https://ecommerceapi.com';
-          if (event.authorization) {
-              console.log('Adding username and roles to login tokens.')
-              api.idToken.setCustomClaim('preferred_username', event.user.email);
-              api.idToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
-              api.accessToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
-          }
-      }
-    ```
-6. In Spring Boot application, add the following Gradle/Maven dependencies:
-```
-implementation("com.okta.spring:okta-spring-boot-starter:3.0.5")
-implementation("org.springframework.boot:spring-boot-starter-security")
-implementation("org.springframework.boot:spring-boot-starter-web")
-```
-7. Update `application.properties` with the following
-   1. AUTH0_DOMAIN is the domain field of your Auth0 Application
-   2. AUTH0_CLIENT_ID is the Client ID of your Auth0 Application
-   3. AUTH0_CLIENT_SECRET is the Client Secret of your Auth0 Application
-   4. AUTH0_AUDIENCE is the API Audience of your Auth0 API
-   5. AUTH0_TRIGGER_NAMESPACE is the namespace defined in the JS code of your Auth0 Action Trigger
-```
-okta.oauth2.issuer=https://${AUTH0_DOMAIN}/
-okta.oauth2.client-id=${AUTH0_CLIENT_ID}
-okta.oauth2.client-secret=${AUTH0_CLIENT_SECRET}
-okta.oauth2.audience=${AUTH0_AUDIENCE}
-okta.oauth2.groupsClaim=${AUTH0_TRIGGER_NAMESPACE}/roles
-```
-8. Create a `security` package, then create a class within called `SecurityConfiguration` to configure OAuth connection between Spring Boot Application and Auth0:
-```
-package com.example.security;
+package com.dylanwraith.ecommerce.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -150,9 +89,3 @@ public class SecurityConfiguration {
         };
     }
 }
-```
-9. Following this example:
-   1. Endpoint "/" does not require authentication
-   2. Endpoint "/private" (or anything besides "/private-admin") will require authorization, but not authentication
-   3. Endpoint "/private-admin" will require authorization with ADMIN role
-      1. Users can be given roles within Auth0 for testing
